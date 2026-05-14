7fd49a2e119e3029a31ccdb211ddcd7e2f5ba918
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-05-14 12:12:31 +0800

    add few changes and files

--- MyCoinBoxBank/ViewModels/InsertCoinViewModel.cs
+++ MyCoinBoxBank/ViewModels/InsertCoinViewModel.cs
@@ -1,5 +1,31 @@
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 using MyCoinBoxBank.Models;
 using MyCoinBoxBank.Services;
+using MyCoinBoxBank.ViewModels;
+
+namespace MyCoinBank.ViewModels;
+
+public partial class InsertCoinViewModel : ObservableObject
+{
+    private readonly IDatabaseService _db;
+    private readonly Esp32Service _esp32Service;
+
+    [ObservableProperty] private string _statusMessage = "Raedy";
+    [ObservableProperty] private bool _isBusy = "False";
+    [ObservableProperty] private decimal _selectedAmount = 1.00m;
+
+    public InsertCoinViewModel(IDatabaseService db, Esp32Service esp32Service)
+    {
+        _db = db;
+        _esp32Service = esp32Service;
+    }
+
+    public async Task InserCoins()
+    {
+        
+    }
+}
+
